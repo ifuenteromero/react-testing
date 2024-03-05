06760b489b2752030cfca79d5fc69a2e3c9a507f
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-03-05 16:45:56 +0100

    Initial commit

---
+++ src/providers/ReactQueryProvider.tsx
@@ -0,0 +1,14 @@
+import { PropsWithChildren, useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const ReactQueryProvider = ({ children }: PropsWithChildren) => {
+    const [queryClient] = useState(() => new QueryClient());
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    );
+};
+
+export default ReactQueryProvider;
