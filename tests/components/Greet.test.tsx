92e100564596e0853ead46eae2c1f8d141b10835
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-03-05 18:46:05 +0100

    Simplifying test setup: import '@testing-library/jest-dom/vitest'; - add plugin "Testing Library Snippets" itr

--- tests/components/Greet.test.tsx
+++ tests/components/Greet.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import Greet from '../../src/components/Greet';
-import '@testing-library/jest-dom/vitest';
 
 describe('Greet', () => {
     it('should render Hello with the name when name is provided', () => {
