6a400c3dd1b5a63a5f547bd6e63259cbe8d421bd
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-03-18 19:35:35 +0100

    Testing price input

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -116,4 +116,51 @@ describe('ProductForm', () => {
             expect(error).toHaveTextContent(errorMessage);
         }
     );
+
+    it.each([
+        {
+            scenario: 'missing',
+            errorMessage: /required/i,
+        },
+        {
+            scenario: 'greater than 1000',
+            price: 1001,
+            errorMessage: /1000/i,
+        },
+        {
+            scenario: '0',
+            price: 0,
+            errorMessage: /1/i,
+        },
+        {
+            scenario: 'negative',
+            price: -1,
+            errorMessage: /1/i,
+        },
+        {
+            scenario: 'not a number',
+            price: 'aaa',
+            errorMessage: /required/i,
+        },
+    ])(
+        'should display an error if price is $scenario',
+        async ({ price, errorMessage }) => {
+            const { waitForFormToLoad, user } = renderComponent();
+            const { nameInput, priceInput, categoriesInput, submitButton } =
+                await waitForFormToLoad();
+            await user.type(nameInput, 'aaaaa');
+
+            if (price !== undefined)
+                await user.type(priceInput, price.toString());
+            await user.click(categoriesInput);
+            const options = screen.getAllByRole('option');
+            const option = options[0];
+            await user.click(option);
+            await user.click(submitButton);
+
+            const error = screen.getByRole('alert');
+            expect(error).toBeInTheDocument();
+            expect(error).toHaveTextContent(errorMessage);
+        }
+    );
 });
