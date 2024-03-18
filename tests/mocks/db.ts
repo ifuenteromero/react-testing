13e64e36d8eff16fad7247bd0f085dfbaa496c5a
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-03-18 17:58:10 +0100

    create unique products and categories

--- tests/mocks/db.ts
+++ tests/mocks/db.ts
@@ -4,12 +4,12 @@ import { factory, manyOf, oneOf, primaryKey } from '@mswjs/data';
 export const db = factory({
     category: {
         id: primaryKey(() => faker.number.int()),
-        name: () => faker.commerce.department(),
+        name: () => uniqueCategoryName(),
         product: manyOf('product'),
     },
     product: {
         id: primaryKey(() => faker.number.int()),
-        name: () => faker.commerce.productName(),
+        name: () => uniqueProductName(),
         price: () => faker.number.int({ min: 1, max: 100 }),
         categoryId: () => faker.number.int(),
         category: oneOf('category'),
@@ -24,3 +24,28 @@ export const getProductsByCategory = (categoryId?: number) => {
         },
     });
 };
+
+// Helper functions for generating unique names
+const uniqueCategoryName = (() => {
+    const usedNames = new Set<string>();
+    return () => {
+        let name;
+        do {
+            name = faker.commerce.department();
+        } while (usedNames.has(name));
+        usedNames.add(name);
+        return name;
+    };
+})();
+
+const uniqueProductName = (() => {
+    const usedNames = new Set<string>();
+    return () => {
+        let name;
+        do {
+            name = faker.commerce.productName();
+        } while (usedNames.has(name));
+        usedNames.add(name);
+        return name;
+    };
+})();
